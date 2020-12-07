03cbec28a6a62c7e1140ebadef6fad25bf2c550e
Eric Lunt <eric@lunt.software>
2020-12-06 23:40:42 -0700

    Day 7 revisions and tests

--- day7/main.go
+++ day7/main.go
@@ -24,35 +24,46 @@ func main() {
 	}
 	defer input.Close()
 	rules := readRules(input)
-	rulesMap := make(map[color]rule)
-	for _, rule := range rules {
-		rulesMap[rule.color] = rule
-	}
+	const target = color("shiny gold")
+	fmt.Printf("Part 1. Count = %d\n", countContainedIn(target, rules))
+	fmt.Printf("Part 2. Total contains = %d", countContained(target, rules)-1)
+}
+
+func countContainedIn(target color, rules map[color][]colorAndQty) int {
 	var count int
-	for _, rule := range rules {
-		if bagDeepContains(rule.color, color("shiny gold"), rulesMap) {
+	for bag := range rules {
+		if bagDeepContains(bag, target, rules) {
 			count++
 		}
 	}
-	fmt.Printf("Part 1. Count = %d\n", count)
-	fmt.Printf("Part 2. Total contains = %d", countContained(color("shiny gold"), rulesMap)-1)
+	return count
 }
 
-func bagDeepContains(ruleColor color, targetColor color, rulesMap map[color]rule) bool {
-	containsColors := rulesMap[ruleColor].containsColors()
+func bagDeepContains(bag color, target color, rules map[color][]colorAndQty) bool {
+	// This could be much more efficient if we kept intermediate results
+	containsColors := colorsOnly(rules[bag])
 	for len(containsColors) > 0 {
-		if containsColors[0] == targetColor {
+		if containsColors[0] == target {
 			return true
 		}
-		containsColors = append(containsColors[1:], rulesMap[containsColors[0]].containsColors()...)
+		containsColors = append(containsColors[1:], colorsOnly(rules[containsColors[0]])...)
 	}
 	return false
 }
 
-func countContained(bag color, rulesMap map[color]rule) int {
+func colorsOnly(contents []colorAndQty) []color {
+	var colors []color
+	for _, caq := range contents {
+		colors = append(colors, caq.color)
+	}
+	return colors
+}
+
+// Note: includes the containing bag! You may need to subtract 1 to the answer returned
+func countContained(bag color, rules map[color][]colorAndQty) int {
 	count := 1
-	for _, caq := range rulesMap[bag].contains {
-		count += caq.qty * countContained(caq.color, rulesMap)
+	for _, caq := range rules[bag] {
+		count += caq.qty * countContained(caq.color, rules)
 	}
 	return count
 }
@@ -64,47 +75,26 @@ type colorAndQty struct {
 	qty int
 }
 
-type rule struct {
-	color
-	contains []colorAndQty
-}
-
-func (r rule) containsColors() []color {
-	var colors []color
-	for _, caq := range r.contains {
-		colors = append(colors, caq.color)
-	}
-	return colors
-}
-
 var (
 	containsRegex = regexp.MustCompile("(\\d+) (.*?) bag")
 )
 
-func readRules(r io.Reader) []rule {
-	var rules []rule
+func readRules(r io.Reader) map[color][]colorAndQty {
+	rules := make(map[color][]colorAndQty)
 	input := bufio.NewScanner(r)
 	for input.Scan() {
-		line := input.Text()
-		var r rule
-		// could use Split
-		bagsIndex := strings.Index(line, " bags")
-		r.color = color(line[:bagsIndex])
-		rightSide := line[bagsIndex:]
-		for _, containsPart := range containsRegex.FindAllStringSubmatch((rightSide), -1) {
+		ruleSides := strings.SplitN(input.Text(), " bags contain ", 2)
+		key := color(ruleSides[0])
+		var contains []colorAndQty
+		for _, containsPart := range containsRegex.FindAllStringSubmatch((ruleSides[1]), -1) {
 			// [1] is the qty, while [2] is the color
 			qty, err := strconv.Atoi(containsPart[1])
 			if err != nil {
 				panic(err)
 			}
-			c := containsPart[2]
-			r.contains = append(r.contains, colorAndQty{color(c), qty})
+			contains = append(contains, colorAndQty{color(containsPart[2]), qty})
 		}
-		rules = append(rules, r)
+		rules[key] = contains
 	}
 	return rules
-}
-
-func (r rule) String() string {
-	return fmt.Sprintf("%s bags contain %v", r.color, r.contains)
 }
--- day7/main_test.go
+++ day7/main_test.go
@@ -1,2 +1,72 @@
 // Advent of Code 2020, Day 7
 package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func Test_countContainedIn(t *testing.T) {
+	type args struct {
+		target color
+		rules  map[color][]colorAndQty
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"example", args{color("shiny gold"), readRules(strings.NewReader(`light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.`))}, 4},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countContainedIn(tt.args.target, tt.args.rules); got != tt.want {
+				t.Errorf("countContainedIn() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_countContained(t *testing.T) {
+	type args struct {
+		bag   color
+		rules map[color][]colorAndQty
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{"example1", args{color("shiny gold"), readRules(strings.NewReader(`light red bags contain 1 bright white bag, 2 muted yellow bags.
+dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+bright white bags contain 1 shiny gold bag.
+muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+faded blue bags contain no other bags.
+dotted black bags contain no other bags.`))}, 33},
+		{"example2", args{color("shiny gold"), readRules(strings.NewReader(`shiny gold bags contain 2 dark red bags.
+dark red bags contain 2 dark orange bags.
+dark orange bags contain 2 dark yellow bags.
+dark yellow bags contain 2 dark green bags.
+dark green bags contain 2 dark blue bags.
+dark blue bags contain 2 dark violet bags.
+dark violet bags contain no other bags.`))}, 127},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := countContained(tt.args.bag, tt.args.rules); got != tt.want {
+				t.Errorf("countContained() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
