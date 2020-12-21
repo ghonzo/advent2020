76505bff49ba0392bedd2de3c9cb75a0b7f36020
Eric Lunt <eric@lunt.software>
2020-12-20 20:09:25 -0700

    Day 20 take 2, full answer

--- day20b/main.go
+++ day20b/main.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"fmt"
 	"io"
 	"os"
@@ -12,7 +13,7 @@ import (
 
 // Day 20: Jurassic Jigsaw (Take 2)
 // Part 1 answer: 64802175715999
-// Part 2 answer:
+// Part 2 answer: 2146
 func main() {
 	fmt.Println("Advent of Code 2020, Day 20b")
 	const filename = "input.txt"
@@ -24,12 +25,13 @@ func main() {
 	defer input.Close()
 	tiles := readTiles(input)
 	fmt.Printf("Part 1. Answer = %d\n", part1(tiles))
+	fmt.Printf("Part 2. Answer = %d\n", part2(tiles))
 }
 
 type transform func(i, j int) (int, int)
 
 // size of the tile
-const n = 10
+var n = 10
 
 // TRANSFORMS
 
@@ -252,4 +254,96 @@ func (t *tile) findEdge(edge int, d direction) bool {
 	// No dice (not sure if we need to set this back but whatevs)
 	t.xform = identity
 	return false
+}
+
+func (t *tile) get(i, j int) byte {
+	ti, tj := t.xform(i, j)
+	return t.data[tj][ti]
+}
+
+var monster = [3][]byte{
+	[]byte("                  # "),
+	[]byte("#    ##    ##    ###"),
+	[]byte(" #  #  #  #  #  #   "),
+}
+
+const monsterCovers = 15
+
+func part2(tiles map[int]*tile) int {
+	// There is probably a fancier way to do this, but I'm going to copy all the subtiles into one big tile
+	var bigTileData [][]byte
+	rowStartTile := findNWTile(tiles)
+	var bigTileDataRow int
+	for t := rowStartTile; t != nullTile; {
+		for j := 1; j < n-1; j++ {
+			var row []byte
+			for i := 1; i < n-1; i++ {
+				row = append(row, t.get(i, j))
+			}
+			bigTileData = append(bigTileData, row)
+		}
+		for t = t.adjacent[east]; t != nullTile; t = t.adjacent[east] {
+			for j := 1; j < n-1; j++ {
+				for i := 1; i < n-1; i++ {
+					bigTileData[bigTileDataRow+j-1] = append(bigTileData[bigTileDataRow+j-1], t.get(i, j))
+				}
+			}
+		}
+		// Advance to the next "tile row"
+		rowStartTile = rowStartTile.adjacent[south]
+		t = rowStartTile
+		bigTileDataRow += (n - 2)
+	}
+	// Okay now we have a big huge tile
+	for _, row := range bigTileData {
+		fmt.Println(string(row))
+	}
+	// Count all the #
+	var count int
+	for _, row := range bigTileData {
+		count += bytes.Count(row, []byte("#"))
+	}
+	// Let's define a big tile for our big tile data
+	bigTile := newTile()
+	bigTile.data = bigTileData
+	// Oh dear God please forgive this hack
+	n = len(bigTileData)
+	// Okay let's go hunting for monsters
+	var monsters int
+	for _, xf := range allTransforms {
+		bigTile.xform = xf
+		// Now define a moving window to find the monster
+		for j := 0; j <= n-len(monster); j++ {
+			for i := 0; i <= n-len(monster[0]); i++ {
+				if detectMonster(bigTile, i, j) {
+					monsters++
+				}
+			}
+		}
+		if monsters > 0 {
+			fmt.Printf("Found %d monsters for xform %v\n", monsters, xf)
+			return count - monsters*15 // 15 is the number of lit pixels in the monster
+		}
+	}
+	panic("No monsters found")
+}
+
+func findNWTile(tiles map[int]*tile) *tile {
+	for _, t := range tiles {
+		if t.adjacent[north] == nullTile && t.adjacent[west] == nullTile {
+			return t
+		}
+	}
+	panic("No NW tile found")
+}
+
+func detectMonster(t *tile, i, j int) bool {
+	for mj, mrow := range monster {
+		for mi, pixel := range mrow {
+			if pixel == '#' && t.get(i+mi, j+mj) != '#' {
+				return false
+			}
+		}
+	}
+	return true
 }
